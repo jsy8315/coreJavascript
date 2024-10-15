d34a22918a6025361b3afa2dc1dc7353a03b10ea
jsy8315 <tdhitbtd@gmail.com>
2024-10-15 19:35:59 +0900

    06. 함수 심화학습 05.객체로서의 함수와 기명 함수 표현식_풀이01

---
+++ assignment/06_advancedFunction/06.05.FunctionAsAnObjectNamedFunctionExpression.js
@@ -0,0 +1,23 @@
+// 06.05. 객체로서의 함수와 기명 함수 표현식
+// 01. 숫자 설정과 감소가 가능한 counter 만들기
+function makeCounter() {
+    let count = 0;
+  
+    function counter() {
+        return counter.count++;
+    }
+    return counter;
+  }
+  
+  let counter = makeCounter();
+  
+  alert( counter() ); // 0
+  alert( counter() ); // 1
+  
+//   counter.set(10); // set the new count
+  
+//   alert( counter() ); // 10
+  
+//   counter.decrease(); // decrease the count by 1
+  
+//   alert( counter() ); // 10 (instead of 11)
